37317981124ea4b930aafc07d345cbc1e60ff472
ppidcard <tvxqlikei@live.cn>
2021-06-25 08:29:43 +1000

    done

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,9 @@
 # Use this space to code along with the readme
 
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+
+def greeting 
+    phrase = "Hello World!"
+    puts phrase
+end
+puts greeting
